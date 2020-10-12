f3f4c5e22597c2714c5ed90451fff5ecc01ac633
Fabien Potencier <fabien@potencier.org>
2020-10-12 09:24:28 +0200

    Make default field provider aware of the field types

--- src/Provider/FieldProvider.php
+++ src/Provider/FieldProvider.php
@@ -4,6 +4,8 @@ namespace EasyCorp\Bundle\EasyAdminBundle\Provider;
 
 use Doctrine\DBAL\Types\Types;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 
 /**
@@ -38,19 +40,35 @@ final class FieldProvider
             Crud::PAGE_DETAIL => [],
         ];
 
+        $fields = [];
         foreach ($entityDto->getAllPropertyNames() as $propertyName) {
             $metadata = $entityDto->getPropertyMetadata($propertyName);
-            if (!\in_array($propertyName, $excludedPropertyNames[$pageName], true) && !\in_array($metadata->get('type'), $excludedPropertyTypes[$pageName], true)) {
-                $defaultPropertyNames[] = $propertyName;
+            $type = $metadata->get('type');
+            if (\in_array($propertyName, $excludedPropertyNames[$pageName], true) || \in_array($type, $excludedPropertyTypes[$pageName], true)) {
+                continue;
+            }
+
+            switch ($type) {
+                case Types::DATE_IMMUTABLE:
+                case Types::DATE_MUTABLE:
+                case Types::DATETIME_IMMUTABLE:
+                case Types::DATETIME_MUTABLE:
+                case Types::DATETIMETZ_IMMUTABLE:
+                case Types::DATETIMETZ_MUTABLE:
+                    $fields[] = DateField::new($propertyName);
+                    break;
+                case Types::BOOLEAN:
+                    $fields[] = BooleanField::new($propertyName);
+                    break;
+                default:
+                    $fields[] = Field::new($propertyName);
+            }
+
+            if (\count($fields) === $maxNumProperties) {
+                break;
             }
         }
 
-        if (\count($defaultPropertyNames) > $maxNumProperties) {
-            $defaultPropertyNames = \array_slice($defaultPropertyNames, 0, $maxNumProperties, true);
-        }
-
-        return array_map(static function (string $fieldName) {
-            return Field::new($fieldName);
-        }, $defaultPropertyNames);
+        return $fields;
     }
 }
